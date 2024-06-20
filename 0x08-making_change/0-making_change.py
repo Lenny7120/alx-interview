acd90aa8d9cf67a50e4ce179cf650b318d3fd9d1
Lenny7120 <ocrank20@gmail.com>
2024-06-20 09:13:44 -0700

    solution

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -4,20 +4,20 @@
 
 
    def makeChange(coins, total):
-           """This function will take a list of coins and use
+       """This function will take a list of coins and use
                   that to calculate how much change the total will require
-                      """
-                          if total <= 0:
-                                      return 0
+        """
+        if total <= 0:
+            return 0
 
-                                      else:
-                                                  coin = sorted(coins)
-                                                          coin.reverse()
-                                                                  counter = 0
-                                                                          for i in coin:
-                                                                                          while(total >= i):
-                                                                                                              counter += 1
-                                                                                                                              total -= i
-                                                                                                                                      if total == 0:
-                                                                                                                                                      return counter
-                                                                                                                                                          return -1
+        else:
+            coin = sorted(coins)
+            coin.reverse()
+            counter = 0
+            for i in coin:
+                while(total >= i):
+                    counter += 1
+                    total -= i
+            if total == 0:
+                return counter
+            return -1
